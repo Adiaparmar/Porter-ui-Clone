82cc1aff3b0b96a22f729aa3b9cd05d9688ed031
Aditya Kumar Singh <adiaparmar@gmail.com>
2023-11-28 19:20:17 +0530

    add js

--- app.js
+++ app.js
@@ -1,159 +1,17 @@
-const arrOne = document.querySelector(".one");
-const ansOne = document.querySelector(".one-a");
+let ans = document.querySelectorAll(".q-ans");
+let ques = document.querySelectorAll(".ques-container");
+let arrow = document.querySelectorAll(".arr");
+let flag = true;
 
-var flag = 0
-
-arrOne.addEventListener("click", function(){
-    if(flag == 0){
-        ansOne.style.display = "block";
-        flag = 1
-    } else {
-        ansOne.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrTwo = document.querySelector(".two");
-const ansTwo = document.querySelector(".two-a");
-
-var flag = 0
-
-arrTwo.addEventListener("click", function(){
-    if(flag == 0){
-        ansTwo.style.display = "block";
-        flag = 1
-    } else {
-        ansTwo.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrThree = document.querySelector(".three");
-const ansThree = document.querySelector(".three-a");
-
-var flag = 0
-
-arrThree.addEventListener("click", function(){
-    if(flag == 0){
-        ansThree.style.display = "block";
-        flag = 1
-    } else {
-        ansThree.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrFour = document.querySelector(".four");
-const ansFour = document.querySelector(".four-a");
-
-var flag = 0
-
-arrFour.addEventListener("click", function(){
-    if(flag == 0){
-        ansFour.style.display = "block";
-        flag = 1
-    } else {
-        ansFour.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrFive = document.querySelector(".five");
-const ansFive = document.querySelector(".five-a");
-
-var flag = 0
-
-arrFive.addEventListener("click", function(){
-    if(flag == 0){
-        ansFive.style.display = "block";
-        flag = 1
-    } else {
-        ansFive.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrSix = document.querySelector(".six");
-const ansSix = document.querySelector(".six-a");
-
-var flag = 0
-
-arrSix.addEventListener("click", function(){
-    if(flag == 0){
-        ansSix.style.display = "block";
-        flag = 1
-    } else {
-        ansSix.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrSeven = document.querySelector(".seven");
-const ansSeven = document.querySelector(".seven-a");
-
-var flag = 0
-
-arrSeven.addEventListener("click", function(){
-    if(flag == 0){
-        ansSeven.style.display = "block";
-        flag = 1
-    } else {
-        ansSeven.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrEight = document.querySelector(".eight");
-const ansEight = document.querySelector(".eight-a");
-
-var flag = 0
-
-arrEight.addEventListener("click", function(){
-    if(flag == 0){
-        ansEight.style.display = "block";
-        flag = 1
-    } else {
-        ansEight.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrNine = document.querySelector(".nine");
-const ansNine = document.querySelector(".nine-a");
-
-var flag = 0
-
-arrNine.addEventListener("click", function(){
-    if(flag == 0){
-        ansNine.style.display = "block";
-        flag = 1
-    } else {
-        ansNine.style.display = "none";
-        flag = 0
-    }
-    
-})
-
-const arrTen = document.querySelector(".ten");
-const ansTen = document.querySelector(".ten-a");
-
-var flag = 0
-
-arrTen.addEventListener("click", function(){
-    if(flag == 0){
-        ansTen.style.display = "block";
-        flag = 1
-    } else {
-        ansTen.style.display = "none";
-        flag = 0
-    }
-    
+Array.from(arrow).forEach(function (e, index) {
+    e.addEventListener("click", function () {
+        if (flag === true ) {
+            ans[index].style.display = "block";
+            flag = false;
+            console.log("hello");
+        } else {
+            ans[index].style.display = "none";
+            flag = true;
+        }
+    })
 })
